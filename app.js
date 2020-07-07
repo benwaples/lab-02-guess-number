abb6cbcc3e809999e861a4b2c0b4071a511d4396
bwapes <benwaples@gmail.com>
2020-07-07 14:00:52 -0700

    collected DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
-
+const getTicket = document.querySelector('.getTicket');
+const guessNumber = document.querySelector('.guessNumber');
+const remainingGuesses = document.querySelector('.remainingGuesses');
+const tooHighOrLow = document.querySelector('.tooHighOrLow');
+const playAgain = document.querySelector('.playAgain');
+const gamesWon = document.querySelector('.gamesWon');
+const gamesLost = document.querySelector('.gamesLost');
 // initialize state
 
-// set event listeners to update state and DOM
+// set event listeners to update state and DOM 
